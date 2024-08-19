52d45308f4fe476e618f8a6f812d02fe1d993ac1
Mubashar <maw8051@rit.edu>
2024-08-18 20:29:40 -0400

    Changing

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -89,7 +89,7 @@ export const HeroSection = () => {
           </button>
           <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
             <span>👋</span>
-            <span className="font-semibold ">Let&apos;s Connect</span>
+            <span className="font-semibold ">Let&apos;s Connect!</span>
           </button>
         </div>
       </div>
