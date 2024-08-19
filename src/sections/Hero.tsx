d3330981d7fd83d793397376c68a2c60c96709c7
Mubashar <maw8051@rit.edu>
2024-08-18 22:18:47 -0400

    pushing

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -74,6 +74,9 @@ export const HeroSection = () => {
           </div>
         </div>
         <div className="max-w-lg mx-auto">
+        <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Mubashar Warriach
+          </h1>
           <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
             Building Exceptional User Experiences
           </h1>
