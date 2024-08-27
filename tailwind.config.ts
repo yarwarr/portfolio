1a8f2e5d77ec8925823f7ff76702d1df0fe26d66
Mubashar <maw8051@rit.edu>
2024-08-26 20:02:26 -0400

    Added a ping

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,9 +22,20 @@ const config: Config = {
     },
     extend: {
       fontFamily: {
-        sans: 'var(--font-sans)',
-        serif: 'var(--font-serif)',
-      }
+        sans: "var(--font-sans)",
+        serif: "var(--font-serif)",
+      },
+      animation: {
+        "ping-large": "ping-large 1s ease-in-out infinite",
+      },
+      keyframes: {
+        "ping-large": {
+          "75%, 100%": {
+            transform: "scale(3)",
+            opacity: "0",
+          },
+        },
+      },
     },
   },
   plugins: [],
